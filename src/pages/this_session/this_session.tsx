c6230102810a863ee37df1da823ca77a2890aa73
s000ik <s000ik@users.noreply.github.com>
2024-12-29 04:36:35 +0530

    Scrollbar present on this session

--- src/pages/this_session/this_session.tsx
+++ src/pages/this_session/this_session.tsx
@@ -19,43 +19,54 @@ export const ThisSession: React.FC<ThisSessionProps> = ({
 
   return (
     <main className={session_style.container}>
-      <div className={session_style.contentSection}>
-        <div className={session_style.sectionText}>
-          <span className={session_style.sectionHead}>Total Browsing Time</span>
-          <span className={session_style.sectionSubhead}>
-            Total screen-time this session
-          </span>
+
+      {/* Fixed content */}
+      <div className={session_style.fixedContent}>
+
+        <div className={session_style.contentSection}>
+          <div className={session_style.sectionText}>
+            <span className={session_style.sectionHead}>Total Browsing Time</span>
+            <span className={session_style.sectionSubhead}>
+              Total screen-time this session
+            </span>
+          </div>
+          <div className={session_style.sectionValue}>
+            <span className={session_style.purpleLargeRoboto}>{formatTime(totalTime)}</span>
+          </div>
         </div>
-        <div className={session_style.sectionValue}>
-          <span className={session_style.purpleLargeRoboto}>{formatTime(totalTime)}</span>
+  
+        <hr className={session_style.divider} />
+  
+        <div className={session_style.contentSection}>
+          <div className={session_style.sectionText}>
+            <span className={session_style.sectionHead}>Top Viewed Sites</span>
+            <span className={session_style.sectionSubhead}>
+              Most viewed sites this browsing session
+            </span>
+          </div>
         </div>
+
       </div>
+  
+      {/* Scrollable content */}
+      <div className={session_style.scrollableContainer}>
 
-      <hr className={session_style.divider} />
-
-      <div className={session_style.contentSection}>
-        <div className={session_style.sectionText}>
-          <span className={session_style.sectionHead}>Top Viewed Sites</span>
-          <span className={session_style.sectionSubhead}>
-            Most viewed sites this browsing session
-          </span>
-        </div>
-      </div>
-
-      <div className={session_style.siteList}>
-        {Object.entries(sessionTimings)
-          .sort(([, timeA], [, timeB]) => timeB - timeA)
-          .map(([hostname, siteTime]) => (
-            <div key={hostname} className={session_style.siteItem}>
-              <div className={session_style.siteItemContent}>
-                <div className={session_style.siteCardContainer}>
-                  <SiteCard hostname={hostname} useIcon={true} />
+        <div className={session_style.siteList}>
+          {Object.entries(sessionTimings)
+            .sort(([, timeA], [, timeB]) => timeB - timeA)
+            .map(([hostname, siteTime]) => (
+              <div key={hostname} className={session_style.siteItem}>
+                <div className={session_style.siteItemContent}>
+                  <div className={session_style.siteCardContainer}>
+                    <SiteCard hostname={hostname} useIcon={true} />
+                  </div>
+                  <span className={session_style.siteCardTime}>{formatTime(siteTime)}</span>
                 </div>
-                <span className={session_style.siteCardTime}>{formatTime(siteTime)}</span>
+                <ProgressBar percentage={(siteTime / totalTime) * 100} />
               </div>
-              <ProgressBar percentage={(siteTime / totalTime) * 100} />
-            </div>
-          ))}
+            ))}
+        </div>
+        
       </div>
     </main>
   );
