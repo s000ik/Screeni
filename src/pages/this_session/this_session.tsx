6dee95b3e7f7ecb067c5c7d63191569b9fb4409f
P4R1H <damps.home@gmail.com>
2024-12-29 01:55:46 +0530

    Common subheader

--- src/pages/this_session/this_session.tsx
+++ src/pages/this_session/this_session.tsx
@@ -11,19 +11,26 @@ export const ThisSession: React.FC<ThisSessionProps> = ({ sessionTimings, format
   const totalSessionTime = Object.values(sessionTimings).reduce((acc, time) => acc + time, 0);
 
   return (
-    <main className="content">
-      <div className="total-time">
-        <span className="label">Total browsing time</span>
-        <span className="value purple-large-roboto">{formatTime(totalSessionTime)}</span>
+    <main className="this-session-container">
+      <div className="content-section">
+        <div className="section-text">
+          <span className="section-head">Total Browsing Time</span>
+          <span className="section-subhead">Total screen-time this session</span>
+        </div>
+        <div className="section-value purple-large-roboto">
+          {formatTime(totalSessionTime)}
+        </div>
       </div>
+  
       <hr className="divider" />
+  
       <div className="most-viewed">
         <h2 className="section-title">Most viewed sites</h2>
         <div className="site-list with-icons">
           {Object.entries(sessionTimings)
             .sort(([, a], [, b]) => b - a)
             .map(([hostname]) => (
-              <SiteCard hostname={hostname} useIcon={true} />
+              <SiteCard hostname={hostname} useIcon={true} key={hostname} />
             ))}
         </div>
       </div>
